0f0d70bab531cc12b142c2306f237fd149a44369
puskar <puskarrajkarki1234@gmail.com>
2021-03-18 16:38:53 +0545

    reverse string in python

---
+++ W3Resource/loop/exercise3.py
@@ -0,0 +1,30 @@
+'''How do you reverse a string in python'''
+str = 'puskar'
+reversestr = str[::-1]
+print(reversestr)
+
+''' This is a method 1'''
+
+word = 'puskar'
+reversedword= word[::-1]
+print(reversestr)
+
+'''another word for reversing string in python 3'''
+
+''' To reverse a string in python'''
+
+''' syntax :
+To reverse a string using slicing, write:
+
+stringname[stringlength::-1] # method 1 '''
+
+'''step1 : first let us create one variable called str'''
+str = 'python programming is the best programming language to learn'
+
+'''step2: Now we declare the variable stringlength to find the length of the above string'''
+
+stringlength = len(str)
+
+reversedstring = str[stringlength::-1]
+
+print(reversedstring)
