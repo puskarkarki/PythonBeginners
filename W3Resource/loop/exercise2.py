0a0d2c1ac2eab47991a3bf9f10c8598cca26df62
puskar <puskarrajkarki1234@gmail.com>
2021-03-18 16:27:32 +0545

    exercise2.py

---
+++ W3Resource/loop/exercise2.py
@@ -0,0 +1,35 @@
+
+'''Write a Python program to construct the following pattern, using a nested for loop.
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+* '''
+
+n = 5
+for i in range(n):
+    for j in range(i):
+        print('*', end="")
+    print('')
+
+for i in range(n,0, -1):
+    for j in range(i):
+        print('*', end='')
+    print('')
+
+n = 7
+for i in range(n):
+    for j in range(i):
+        print('*', end='')
+    print('')
+
+for i in range(n, 0, -1):
+    for j in range(i):
+        print('*', end='')
+    print('')
+
