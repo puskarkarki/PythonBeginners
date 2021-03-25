08c92b239c6a865e3e530efca092f52fb3db49b5
puskar <puskarrajkarki1234@gmail.com>
2021-03-25 15:51:59 +0545

    new exercise uploaded

--- Function/docstring.py
+++ Function/docstring.py
@@ -6,8 +6,43 @@ def square(n):
 
     return square_num
 
-
-
-
 square(9)
 print(square(9))
+
+
+def hello(name):
+
+    "Hello  Ram, I am learning python programming"
+
+    print("hello, " + name + ", How are you")
+
+print(hello.__doc__)
+hello('ram')
+
+''' example 3'''
+
+def greet(name):
+
+    ''' This is a new era of a programming language '''
+
+    print("hello," + name + ", my name is python programming")
+
+print(greet.__doc__)
+greet('puskar')
+
+
+def hi_hello():
+
+    '''Hello Jack, I am learning python programming'''
+
+print(hi_hello.__doc__)
+
+
+def say_hello(name):
+
+    '''I am  mr sujan dahal '''
+
+    print("Hello, " + name + ", I am new to python programming")
+print(say_hello.__doc__)
+say_hello('Prajwol')
+
