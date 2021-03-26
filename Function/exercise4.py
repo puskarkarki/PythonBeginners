fc016d8d896e61210a84410d62a28cd4c3e717e8
puskar <puskarrajkarki1234@gmail.com>
2021-03-26 17:12:24 +0545

    sum of two variables using function

---
+++ Function/exercise4.py
@@ -0,0 +1,25 @@
+def sum(num1, num2):
+
+    sum_num = num1 + num2
+
+    return sum_num
+
+num1 = int(input("Enter the value of the first number: "))
+num2 = int(input("Enter the value of the second number: "))
+
+print("sum = ", sum(num1, num2))
+
+''' second way to add the sum of two variables using function'''
+
+def sum(a,b):
+
+    return a +b
+
+a = 12
+b = 23
+
+print(sum(a,b))
+
+
+
+
