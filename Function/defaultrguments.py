259f931b4e8359589467bd07577df90ff4b307f1
puskar <puskarrajkarki1234@gmail.com>
2021-03-26 19:58:11 +0545

    defaultargumentexample

---
+++ Function/defaultrguments.py
@@ -0,0 +1,15 @@
+''' Examples for python default arguments'''
+
+def person(name, age = '26', height = '5.10', sex = 'male'):
+
+    message = ("Hi I am  " + name +  " I am "  + age + " years old " + "my height is " + height + "and I am " + sex)
+
+    print("See the message:", message)
+
+    return message
+
+
+
+person(name='Janak,')
+
+
